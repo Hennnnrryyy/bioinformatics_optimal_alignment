3c38646deb1ac5d48d43a61e905452525cb92823
HenpaiSensei <henrysmith@ufl.com>
2019-11-07 13:15:48 -0500

    Added comments

--- assignment2.py
+++ assignment2.py
@@ -11,7 +11,7 @@ def main():
     print(multipleAlignments)
     return 0
 
-def getSequences(inputFile):
+def getSequences(inputFile): #Gets sequences from input file
     global sequence_1
     global sequence_2
     input = open(inputFile, "r")
@@ -19,42 +19,42 @@ def getSequences(inputFile):
     sequence_2 = input.readline()
     input.close()
 
-def initializeMatrix():
+def initializeMatrix(): # Initializes and fills in matrix
     global grid, gap, match, mismatch
     gap = -2
     match = 2
     mismatch = -1
     grid = [ [0]*len(sequence_1) for i in range(len(sequence_2))]
     
-    accumulator = gap #Used for initializing first row and column
+    accumulator = gap # Used for initializing first row and column
 
-    for i in range(1, len(sequence_2)): #Initializes 1st row
+    for i in range(1, len(sequence_2)): # Initializes 1st row
         grid[0][i] = accumulator
         accumulator += gap
     
     accumulator = gap
-    for i in range(1, len(sequence_1)): #Initializes 1st column
+    for i in range(1, len(sequence_1)): # Initializes 1st column
         grid[i][0] = accumulator
         accumulator += gap
 
-    fillInMatrix()
+    fillInMatrix() # Fill in other rows and columns
 
-def getMax(row,col): #Finds max out of the left element, above element, and upper left diagonal element
+def getMax(row,col): # Finds max out of the left element, above element, and upper left diagonal element with match or gap
     return max(grid[row][col-1]+gap, grid[row-1][col]+gap, grid[row-1][col-1]+getMatch(row,col))
 
-def getMatch(row, col):
+def getMatch(row, col): # Determines if two letters in sequence are a match
     return match if sequence_1[col-1:col] == sequence_2[row-1:row] else mismatch
 
-def fillInMatrix():
+def fillInMatrix(): # Fills in matrix by calculating score of each cell based on the max of its adjacent cells
     for row in range(1, len(sequence_2)) :
         for col in range(1, len(sequence_1)) :
             score = getMax(row,col)
             grid[row][col] = score
 
-def getOptimalScore():
+def getOptimalScore(): # Retrieves optimal score from the last cell
     return grid[len(sequence_2)-1][len(sequence_1)-1]
 
-def getOptimalAlignments():
+def getOptimalAlignments(): # Back tracing to find ONE optimal alignment
     global optimal_sequence_1, optimal_sequence_2, multipleAlignments
     row = len(sequence_2)-1
     col = len(sequence_1)-1
@@ -63,34 +63,36 @@ def getOptimalAlignments():
     multipleAlignments = "NO"
 
     while row >= 1 and col >= 1:
-        m = getMax(row, col)
-        if ( 
+        m = getMax(row, col) # Max out of adjacent values
+
+        if ( multipleAlignments == "NO" and # Checks if two max values exist and can be used to find multiple optimal alignments
             ( (getMax(row,col) == grid[row][col-1]+gap) and (getMax(row,col) == grid[row-1][col]+gap) ) or
             ( (getMax(row,col) == grid[row][col-1]+gap) and (getMax(row,col) == grid[row-1][col-1]+gap) ) or
             ( (getMax(row,col) == grid[row-1][col]+gap) and (getMax(row,col) == grid[row-1][col]+gap) )
             ):
                 multipleAlignments = "YES"
+
         if m == grid[row][col-1]+gap: # Left
-            optimal_sequence_1 += sequence_1[col-1:col]
-            optimal_sequence_2 += "-"
+            optimal_sequence_1 += sequence_1[col-1:col] # Keep letter
+            optimal_sequence_2 += "-" # Gap
             col -= 1
 
-        elif m == grid[row-1][col]+gap: #Above
-            optimal_sequence_1 += "-"
-            optimal_sequence_2 += sequence_2[row-1:row]
+        elif m == grid[row-1][col]+gap: # Above
+            optimal_sequence_1 += "-" # Gap
+            optimal_sequence_2 += sequence_2[row-1:row] # Keep letter
             row -= 1
 
-        else:
-            optimal_sequence_1 += sequence_1[col-1:col]
-            optimal_sequence_2 += sequence_2[row-1:row]
+        else: # Upper Left Diagonal
+            optimal_sequence_1 += sequence_1[col-1:col] # Keep letter
+            optimal_sequence_2 += sequence_2[row-1:row] # Keep letter
             col -= 1
             row -= 1
-    optimal_sequence_1 = optimal_sequence_1[::-1]
-    optimal_sequence_2 = optimal_sequence_2[::-1]
+    optimal_sequence_1 = optimal_sequence_1[::-1] # Reverse optimal sequence 1
+    optimal_sequence_2 = optimal_sequence_2[::-1] # Reverse optimal sequence 2
 
 
 
-def printPrettyMatrix():
+def printPrettyMatrix(): # Prints matrix in a pretty fashion
     for x in range(0, len(sequence_2)):
         for y in range(0, len(sequence_1)):
             print ("%d\t" %(grid[x][y])),
